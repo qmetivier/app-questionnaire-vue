c791141d16933adfdef0d8cb0c9d4ea35a73dc91
Quentin Metivier <quentin.metivier@consertotech.pro>
2019-10-14 15:24:40 +0200

    Create Page Login

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
-import App from './App.vue'
+import QM_Login from './pages/QM_Login.vue'
+import QM_Questionnaire from './pages/Questionnaire.vue'
 
 Vue.config.productionTip = false
 
+const NotFound = { template: '<p>Page not found</p>' }
+
+const routes = {
+    '/': QM_Login,
+    '/questionnaire' : QM_Questionnaire,
+};
+
 new Vue({
-  render: h => h(App),
+    el: '#app',
+    data: {
+        currentRoute: window.location.pathname,
+    },
+    computed: {
+        ViewComponent () {
+            return routes[this.currentRoute] || NotFound;
+        }
+    },
+    render (h) { return h(this.ViewComponent) }
 }).$mount('#app')
---
+++ src/pages/QM_Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="login_div">
+    <img src="../assets/checklist.png" alt="Questionnaire" class="img_questionnaire">
+    <div id="Form_Login">
+      <div id="ErrorMessage" v-text="QM_errorMessage"></div>
+      <label for="firstName_login" class="labelForm">Prénom :
+        <input type="text" id="firstName_login" v-model="QM_firstName">
+      </label>
+      <label for="lasName_login" class="labelForm">Nom :
+        <input type="text" id="lasName_login" v-model="QM_lastName">
+      </label>
+      <label for="company_login" class="labelForm">Entreprise :
+        <input type="text" id="company_login" v-model="QM_company">
+      </label>
+      <button id="button_login" v-on:click='QM_login'>Se connecter</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'QM_Login',
+    data() {
+      return{
+        QM_firstName: "",
+        QM_lastName: "",
+        QM_company: "",
+        QM_errorMessage: "",
+      }
+    },
+    methods : {
+        QM_login : function (){
+          if (this.QM_firstName === "" || this.QM_lastName === "" || this.QM_company === ""){
+            this.QM_errorMessage = "Veuillez remplir tout les champs";
+          }else{
+            this.QM_errorMessage = "";
+            window.location.pathname = "/questionnaire";
+          }
+        }
+      }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+  .login_div{
+    text-align: center;
+    margin: 0 auto;
+    width: 95vw;
+    height: 100vh;
+    background-color: ghostwhite;
+  }
+
+  .img_questionnaire{
+    width: 150px;
+  }
+
+  #Form_Login{
+    background-color: lightsteelblue;
+    width: 500px;
+    margin: 0 auto;
+    border: lightsteelblue 2px solid;
+    border-radius: 10px;
+    padding-bottom: 5px;
+    padding-left: 50px;
+    text-align: left;
+  }
+
+  #Form_Login .labelForm{
+    position: relative;
+    color: ghostwhite;
+    display: block;
+    padding: 10px;
+  }
+
+  #Form_Login .labelForm input{
+    position: absolute;
+    top: 20%;
+    right: 20%;
+  }
+
+  #button_login{
+    display: block;
+    margin: 20px auto;
+  }
+
+</style>
